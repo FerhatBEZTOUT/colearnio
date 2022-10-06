39439620e186d6307629c657528ae3414c214fde
weghz <weghzan@gmail.com>
2022-10-06 18:56:27 +0200

    ferhat

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./bootstrap-5.2.2-dist/css/bootstrap.css">
+    <link rel="stylesheet" href="./css/style.css">
+    <title>Colearnio, apprendre ensemble</title>
+</head>
+<body>
+    <div class="container d-flex justify-content-center align-items-center">
+        <h2>En cours de développement</h2>
+    </div>
+</body>
+</html>
